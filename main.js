297240ca239016dc7b43724e9d80be978802718a
ldickmann <ldickmann12@gmail.com>
2025-04-02 16:07:02 -0300

    DOCS: Comentarios adicionados as funcões em main.js & no container com a imagem no dropdown no CSS

--- main.js
+++ main.js
@@ -5,6 +5,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const menuIcon = document.getElementById("menuIcon");
   const navbarMenuTitle = document.querySelector(".navbar-menu-title");
 
+  /**
+   * Exibe o menu suspenso ativando seus elementos.
+   *
+   * - Adiciona a classe "active" ao dropdownMenu para mostrar o menu suspenso.
+   * - Adiciona a classe "active" ao menuToggle para indicar o estado alternado.
+   * - Adiciona a classe "active" ao navbarMenuTitle para destacar o título da barra de navegação.
+   * - Altera a fonte do menuIcon para exibir o ícone de menu azul.
+   */
   function showDropdown() {
     dropdownMenu.classList.add("active");
     menuToggle.classList.add("active");
@@ -12,6 +20,12 @@ document.addEventListener("DOMContentLoaded", () => {
     menuIcon.src = "./assets/icons/menu-icon-blue.svg";
   }
 
+  /**
+   * Oculta o menu suspenso.
+   *
+   * Esta função remove a classe "ativa" dos elementos do menu suspenso, alternância de menu e título do menu da barra de navegação
+   * para garantir que o menu suspenso fique oculto e redefine a fonte do ícone do menu para a imagem padrão.
+   */
   function hideDropdown() {
     dropdownMenu.classList.remove("active");
     menuToggle.classList.remove("active");
