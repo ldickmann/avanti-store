6091d2108f27c1b56d8ed3f6772e4aaff9120a35
ldickmann <ldickmann12@gmail.com>
2025-04-02 07:19:27 -0300

    FEAT & FIX: Ajustes gerais no CSS e no HTML e inclusão das outras listas do dropdown container

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
 // Este script manipula a funcionalidade do menu suspenso para a visualização móvel
 document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("menuToggle");
+  const dropdownMenu = document.getElementById("dropdownMenu");
   const menuIcon = document.getElementById("menuIcon");
-  const dropdownContent = document.getElementById("dropdownContent");
-  const containerDropdown = document.querySelector(".container-dropdown");
   const navbarMenuTitle = document.querySelector(".navbar-menu-title");
 
   function toggleDropdown(event) {
     event.stopPropagation();
-    dropdownContent.classList.toggle("active");
-    containerDropdown.classList.toggle("active");
+    dropdownMenu.classList.toggle("active");
+    menuToggle.classList.toggle("active");
+    navbarMenuTitle.classList.toggle("active");
 
     // Altera o ícone conforme o estado
-    if (containerDropdown.classList.contains("active")) {
+    if (menuToggle.classList.contains("active")) {
       menuIcon.src = "./assets/icons/menu-icon-blue.svg";
     } else {
       menuIcon.src = "./assets/icons/menu-icon.svg";
     }
   }
 
-  menuIcon.addEventListener("click", toggleDropdown);
+  menuToggle.addEventListener("click", toggleDropdown);
   navbarMenuTitle.addEventListener("click", toggleDropdown);
 
   document.addEventListener("click", (event) => {
     if (
-      !menuIcon.contains(event.target) &&
-      !navbarMenuTitle.contains(event.target) &&
-      !dropdownContent.contains(event.target)
+      !menuToggle.contains(event.target) &&
+      !dropdownMenu.contains(event.target)
     ) {
-      dropdownContent.classList.remove("active");
-      containerDropdown.classList.remove("active");
+      dropdownMenu.classList.remove("active");
+      menuToggle.classList.remove("active");
+      navbarMenuTitle.classList.remove("active");
       menuIcon.src = "./assets/icons/menu-icon.svg";
     }
   });
