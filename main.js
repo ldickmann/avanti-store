44f0cbef0380e140ab83a6a071a0c814a2eed350
ldickmann <ldickmann12@gmail.com>
2025-04-02 02:42:35 -0300

    FEAT: Alteração de .svg quando clicado o menu-icon (troca pro icon azul) & estilizações gerais do container-dropdown

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
+// Este script manipula a funcionalidade do menu suspenso para a visualização móvel
 document.addEventListener("DOMContentLoaded", () => {
   const menuIcon = document.getElementById("menuIcon");
   const dropdownContent = document.getElementById("dropdownContent");
+  const containerDropdown = document.querySelector(".container-dropdown");
+  const navbarMenuTitle = document.querySelector(".navbar-menu-title");
 
-  menuIcon.addEventListener("click", (event) => {
+  function toggleDropdown(event) {
     event.stopPropagation();
     dropdownContent.classList.toggle("active");
-  });
+    containerDropdown.classList.toggle("active");
+
+    // Altera o ícone conforme o estado
+    if (containerDropdown.classList.contains("active")) {
+      menuIcon.src = "./assets/icons/menu-icon-blue.svg";
+    } else {
+      menuIcon.src = "./assets/icons/menu-icon.svg";
+    }
+  }
+
+  menuIcon.addEventListener("click", toggleDropdown);
+  navbarMenuTitle.addEventListener("click", toggleDropdown);
 
   document.addEventListener("click", (event) => {
     if (
       !menuIcon.contains(event.target) &&
+      !navbarMenuTitle.contains(event.target) &&
       !dropdownContent.contains(event.target)
     ) {
       dropdownContent.classList.remove("active");
+      containerDropdown.classList.remove("active");
+      menuIcon.src = "./assets/icons/menu-icon.svg";
     }
   });
 });
