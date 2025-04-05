5458f6aa52771fe97d1fd50e317181707c41dcbf
ldickmann <ldickmann12@gmail.com>
2025-04-05 10:36:38 -0300

    REFACTOR & FEAT: icone para abrir o menu nas telas mobile alterado de svg para i & criado evento de click para abrir o menu

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ var dropbtn = document.querySelector(".dropbtn");
 var dropdown = document.getElementById("dropdownAllCategories");
 
 function showDropdown() {
+  // Verificar como adaptar o dropdown para telas menores
+  if (window.innerWidth <= 430) return; // Se a largura da tela for menor ou igual a 430px, não exibe o dropdown
+
   clearTimeout(hideTimeout); // Limpa o timeout se o mouse entrar novamente
   document.getElementById("dropdownAllCategories").classList.add("show");
   // Adiciona a classe "hover" ao botão quando o mouse está sobre ele
@@ -28,3 +31,13 @@ dropbtn.addEventListener("mouseleave", hideDropdown);
 // Adiciona os eventos de mouseenter e mouseleave ao dropdown
 dropdown.addEventListener("mouseenter", showDropdown);
 dropdown.addEventListener("mouseleave", hideDropdown);
+
+/* Manipula o dropdown do icone navbar-menu das telas responsivas até 425px */
+var mobileIcon = document.querySelector(".fa-bars.mobile");
+var navbarMenu = document.querySelector(".navbar-menu");
+
+mobileIcon.addEventListener("click", function () {
+  if (window.innerWidth <= 425) {
+    navbarMenu.classList.toggle("active"); // Alterna a classe "active" no menu da navbar
+  }
+});
