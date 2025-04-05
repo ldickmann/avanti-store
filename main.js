61d2bc8ddc3f51a7604c93232576946a0d37fd58
ldickmann <ldickmann12@gmail.com>
2025-04-05 11:09:24 -0300

    Funções e eventos para o dropdown do button 'Departamento' adicionados

--- main.js
+++ main.js
@@ -32,6 +32,35 @@ dropbtn.addEventListener("mouseleave", hideDropdown);
 dropdown.addEventListener("mouseenter", showDropdown);
 dropdown.addEventListener("mouseleave", hideDropdown);
 
+/* Dropdown do botão "Departamento" */
+var dropbtnDepartamento = document.querySelector(".dropbtn-departamento");
+var dropCategoria = document.getElementById("dropCategoria");
+
+function showDropdownDepartamento() {
+  // Verificar como adaptar o dropdown para telas menores
+  if (window.innerWidth <= 430) return; // Se a largura da tela for menor ou igual a 430px, não exibe o dropdown
+
+  clearTimeout(hideTimeout); // Limpa o timeout se o mouse entrar novamente
+  document.getElementById("dropCategoria").classList.add("show-departamento");
+  // Adiciona a classe "hover" ao botão quando o mouse está sobre ele
+  dropbtnDepartamento.classList.add("hover-active-departamento");
+}
+
+function hideDropdownDepartamento() {
+  hideTimeout = setTimeout(function () {
+    dropCategoria.classList.remove("show-departamento");
+    dropbtnDepartamento.classList.remove("hover-active-departamento");
+  }, 100);
+}
+
+// Adiciona os eventos de mouseenter e mouseleave ao botão e ao dropdown
+dropbtnDepartamento.addEventListener("mouseenter", showDropdownDepartamento);
+dropbtnDepartamento.addEventListener("mouseleave", hideDropdownDepartamento);
+
+// Adiciona os eventos de mouseenter e mouseleave ao dropdown
+dropCategoria.addEventListener("mouseenter", showDropdownDepartamento);
+dropCategoria.addEventListener("mouseleave", hideDropdownDepartamento);
+
 /* Manipula o dropdown do icone navbar-menu das telas responsivas até 425px */
 var mobileIcon = document.querySelector(".fa-bars.mobile");
 var navbarMenu = document.querySelector(".navbar-menu");
